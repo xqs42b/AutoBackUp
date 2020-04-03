684eb90c83de6a379909a2a1395b7a4a7a617e25
bobos008 <501919337@qq.com>
2020-04-03 15:00:01 +0800

    增加服务端，客户端配置文件功能

---
+++ client/config.py
@@ -0,0 +1,12 @@
+# 发送文件端口
+port = 10000
+
+# ip
+server_ip = "172.16.27.1"
+
+# 被保存文件目录最好是绝对路径
+file_path = "/home/xqs/auto_backup/client/"
+
+# 时间间隔（以秒作为单位）
+gap_time = 30
+
---
+++ server/setting.py
@@ -0,0 +1,18 @@
+# 端口
+port = 10000
+
+# 最大连接数
+max_conn = 10000
+
+# ip
+# server_ip = "172.16.27.1"
+server_ip = "0.0.0.0"
+
+# 被保存文件目录最好是绝对路径
+save_path = "/home/xqs/auto_backup/server/files/"
+
+# 客户端删除的文件，服务端保存到垃圾桶
+trash_path = "/home/xqs/auto_backup/server/trash/"
+
+# 垃圾文件保存最长时间(默认七天)
+max_save_time = 604800
